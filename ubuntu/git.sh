85923a440d7a7c632617fb0e16c54ecbd4cfd7ab
hakumaku <gentlebuuny@gmail.com>
2020-05-31 16:21:01 +0900

    ssh key commands added.

--- ubuntu/git.sh
+++ ubuntu/git.sh
@@ -2,5 +2,14 @@
 
 dotfiles="$HOME/workspace/ubuntu-fresh/dotfiles"
 
+set_ssh() {
+	ssh-keygen -t rsa -b 4096 -C "gentlebuuny@gmail.com" &&
+		eval "$(ssh-agent -s)" &&
+		ssh-add ~/.ssh/id_rsa &&
+		git remote set-url origin "https://github.com/hakumaku/ubuntu-fresh" &&
+		xclip -sel clip < ~/.ssh/id_rsa.pub &&
+		firefox https://github.com/settings/ssh/new
+}
+
 sudo apt install git &&
 	(cd && ln -s $dotfiles/git/.gitconfig)
