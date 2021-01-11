2315be9f0bca6b9e527e4b1bd6a0daedd7d16e50
hakumaku <gentlebuuny@gmail.com>
2021-01-11 11:55:26 +0900

    Updated

--- ubuntu/nvim.sh
+++ ubuntu/nvim.sh
@@ -20,7 +20,7 @@ install_plug() {
 	local config="$HOME/.config"
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
-		(mkdir -p $config && ln -s "$dotfile/nvim") && nvim +PlugInstall +qall
+		(mkdir -p $config && cd $config && ln -s "$dotfile/nvim") && nvim +PlugInstall +qall
 }
 
 install_nodejs() {
