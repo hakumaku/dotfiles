d400e6861ec6062317d26bcbd30f6ade736542bf
haku <gentlebuuny@gmail.com>
2021-03-21 19:11:28 +0900

    Updated

--- ubuntu/nvim.sh
+++ ubuntu/nvim.sh
@@ -22,7 +22,7 @@ install_plug() {
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
 		(mkdir -p $config && cd $config && ln -s "$dotfile/nvim") && nvim +PlugInstall +qall &&
-		(nvim +"CocInstall coc-clangd coc-cmake coc-snippets") &&
+		(nvim +"CocInstall coc-clangd coc-cmake coc-snippets" +qall) &&
 		(mkdir -p $config/coc && cd $config/coc && ln -s "$dotfile/ultisnips")
 }
 
