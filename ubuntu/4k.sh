7459d92c6469982f0785384546c8c70c63eeab69
haku <gentlebuuny@gmail.com>
2021-05-01 19:06:53 +0900

    Updated

--- ubuntu/4k.sh
+++ ubuntu/4k.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-# GNOME Shell
-gsettings set org.gnome.desktop.interface scaling-factor 2
-# GDM
-config="/etc/gdm3/greeter.dconf-defaults"
-sudo sed -i '/\[org\/gnome\/desktop\/interface\]/a scaling-factor=uint32 2' $config
+scale_gdm() {
+	local config="/etc/gdm3/greeter.dconf-defaults"
+	sudo sed -i '/\[org\/gnome\/desktop\/interface\]/a scaling-factor=uint32 2' $config
+}
+
+scale_qt() {
+	echo "QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee -a /etc/environment
+	echo "QT_AUTO_SCREEN_SCALE_FACTOR=0" | sudo tee -a /etc/environment
+	echo "QT_SCALE_FACTOR=2.0" | sudo tee -a /etc/environment
+}
+
+scale_gdm && scale_qt
