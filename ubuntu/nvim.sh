bf8248921c3be7345f32874be1cb35569dc8a039
haku <gentlebuuny@gmail.com>
2021-05-05 17:47:53 +0900

    updated

--- ubuntu/nvim.sh
+++ ubuntu/nvim.sh
@@ -19,6 +19,7 @@ install_clang() {
 
 install_plug() {
 	local config="$HOME/.config"
+	python3 -m pip install --user --upgrade pynvim
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
 		(mkdir -p $config && cd $config && ln -s "$dotfile/nvim") && nvim +PlugInstall +qall &&
