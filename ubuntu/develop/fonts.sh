89722c5f05e917926d69c35e1e04c9b3838ffd70
haku <gentlebuuny@gmail.com>
2021-08-01 22:03:53 +0900

    Update

--- ubuntu/develop/fonts.sh
+++ ubuntu/develop/fonts.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+install_meslo() {
+  local fonts=(
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+  )
+  local font_dir="$HOME/.local/share/fonts/MesloLGS"
+  mkdir -p $font_dir
+  for font in "${fonts[@]}"; do
+    wget -q "$font" -P "$font_dir"
+  done
+  fc-cache -fv
+}
+
 install_fonts() {
   local fonts=(
     "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip"
@@ -18,3 +33,4 @@ install_fonts() {
 }
 
 install_fonts
+install_meslo
