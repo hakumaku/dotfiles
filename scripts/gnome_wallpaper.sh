8cb7c3cca1c41ee02a1c3fffdbb5bd19d0c4e903
haku <gentlebuuny@gmail.com>
2021-08-15 08:26:20 +0900

    Update

--- scripts/gnome_wallpaper.sh
+++ scripts/gnome_wallpaper.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 
-shuffle() {
-  local -n arr=$1
-
-  for ((i = 0; i < 300; i++)); do
-    local i1=$(($RANDOM % $size))
-    local i2=$(($RANDOM % $size))
-    local t="${arr[i1]}"
-    arr[i1]="${arr[i2]}"
-    arr[i2]="$t"
-  done
-}
-
 main() {
   readarray -t wallpapers < <(fd . $HOME/Pictures/feh)
   size=${#wallpapers[*]}
 
   while true; do
-    shuffle wallpapers
-    for wallpaper in "${wallpapers[@]}"; do
-      gsettings set org.gnome.desktop.background picture-uri "$wallpaper"
-      gsettings set org.gnome.desktop.background picture-options "scaled"
+    indices=$(seq $size | shuf)
+    for index in ${indices[@]}; do
+      echo gsettings set org.gnome.desktop.background picture-options "scaled"
+      echo gsettings set org.gnome.desktop.background picture-uri "${wallpapers[$index-1]}"
       sleep 1m
     done
   done
