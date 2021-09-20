ce8c6a6f39fe20fce45f2a96db55677c377a902a
hakumaku <gentlebuuny@gmail.com>
2021-09-21 00:52:33 +0900

    Update

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -23,7 +23,6 @@ return require('packer').startup(function(use)
   }
   use {'hrsh7th/nvim-compe', config = function() require("config/compe") end}
   use {'SirVer/ultisnips', config = function() require("config/ultisnips") end}
-  use {'honza/vim-snippets'}
   use {
     'lewis6991/gitsigns.nvim',
     config = function() require("config/gitsigns") end
