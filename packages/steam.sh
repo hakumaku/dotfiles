9ef88e44361c26fb7c7b72ccd4f3bc4df4c47c87
haku <gentlebuuny@gmail.com>
2022-01-23 23:07:59 +0900

    Update

--- packages/steam.sh
+++ packages/steam.sh
@@ -3,17 +3,18 @@
 set -euo pipefail
 
 install_steam() {
-  install_dependencies steam
-  install_package steam
-
   # Enable multilib on Arch Linux.
   if [[ -f /etc/pacman.conf ]]; then
     sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
   fi
+
+  install_dependencies steam
+  install_package steam
+
   # Fix steam tray menu bug (https://github.com/ValveSoftware/steam-for-linux/issues/4428)
   if [[ -f /usr/share/themes/Adwaita/gtk-2.0/main.rc ]]; then
-    sed -i "/direction[[:space:]]*=[[:space:]]*LTR/d" /usr/share/themes/Adwaita/gtk-2.0/main.rc
-    sed -i "/direction[[:space:]]*=[[:space:]]*RTL/d" /usr/share/themes/Adwaita/gtk-2.0/main.rc
+    sudo sed -i "/direction[[:space:]]*=[[:space:]]*LTR/d" /usr/share/themes/Adwaita/gtk-2.0/main.rc
+    sudo sed -i "/direction[[:space:]]*=[[:space:]]*RTL/d" /usr/share/themes/Adwaita/gtk-2.0/main.rc
   fi
 }
 
