b0b417b34ef8445e53623c2ab439f6b9c561969d
haku <gentlebuuny@gmail.com>
2022-01-26 11:05:08 +0900

    Update

--- scripts/gnome_gsettings.sh
+++ scripts/gnome_gsettings.sh
@@ -109,15 +109,15 @@ gsettings set org.gnome.desktop.interface enable-animations true
 
 # Switching to specific workspace
 gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 8
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
-# gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
-# gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
 # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
 gsettings set org.gnome.shell.keybindings switch-to-application-1 "['']"
 gsettings set org.gnome.shell.keybindings switch-to-application-2 "['']"
@@ -125,8 +125,8 @@ gsettings set org.gnome.shell.keybindings switch-to-application-3 "['']"
 gsettings set org.gnome.shell.keybindings switch-to-application-4 "['']"
 gsettings set org.gnome.shell.keybindings switch-to-application-5 "['']"
 gsettings set org.gnome.shell.keybindings switch-to-application-6 "['']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-7 "['']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-8 "['']"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "['']"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "['']"
 # gsettings set org.gnome.shell.keybindings switch-to-application-9 "['']"
 
 # Move window to specific workspace
