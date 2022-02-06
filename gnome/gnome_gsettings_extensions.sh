16f8b2e9871fc63a2b20b62ecbe10f6cd1b655db
haku <gentlebuuny@gmail.com>
2022-02-06 11:44:20 +0900

    Update

--- gnome/gnome_gsettings_extensions.sh
+++ gnome/gnome_gsettings_extensions.sh
@@ -7,29 +7,28 @@ set -eo pipefail
 schemadir() {
   local name="$1"
   local prefix="$XDG_DATA_HOME/gnome-shell/extensions"
-  local output="$(compgen -G $prefix/$name*)"
+  local output="$(compgen -G $prefix/${name}*)"
   echo "$output/schemas"
 }
 
 set_gsettings_extensions() {
-  local name="$1"
-  local schema="--schemadir $(schemadir $name)"
   local extensions=()
-  if [[ ! -d $schema ]]; then
-    echo "Skipping '$name'. (Directory does not exist)"
-  fi
-
   readarray -t extensions < <(jq -r 'keys' $GNOME_EXTENSIONS_SETTINGS | tr -d '[]," ')
 
   for name in ${extensions[@]}; do
+    local schema="$(schemadir $name)"
+    if [[ ! -d "$schema" ]]; then
+      echo "Skipping '$name'. (Directory does not exist)"
+      continue
+    fi
+
     local items=($(jq -r '.["'"$name"'"] | to_entries | map("\(.key)=\(.value|tostring)")|.[]' $GNOME_EXTENSIONS_SETTINGS | tr -d '{}'))
     for item in ${items[@]}; do
       local key=${item%=*}
       local value=${item#*=}
-      gsettings $schema set org.gnome.shell.extensions.$name $key $value
+      gsettings --schemadir $schema set org.gnome.shell.extensions.$name $key $value
     done
   done
 }
 
-set_gsettings_extensions dash-to-dock
-set_gsettings_extensions gtktitlebar
+set_gsettings_extensions
