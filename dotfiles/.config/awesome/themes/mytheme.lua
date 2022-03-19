89a6ae3a53f826e20b2bb3e76c64ca5d44819d2c
haku <gentlebuuny@gmail.com>
2022-03-19 18:53:06 +0900

    Update

--- dotfiles/.config/awesome/themes/mytheme.lua
+++ dotfiles/.config/awesome/themes/mytheme.lua
@@ -1,6 +1,7 @@
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
+local gears = require("gears")
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
 local home = os.getenv("HOME")
@@ -44,34 +45,17 @@ theme.border_marked = "#C678DD"
 
 -- taglist
 theme.taglist_fg_focus    = "#61AFEF"
+theme.taglist_bg_focus    = ""
 theme.taglist_fg_occupied = "#C678DD"
 theme.taglist_fg_urgent   = "#E06C75"
 theme.taglist_fg_empty    = "#ABB2BF"
 theme.taglist_spacing     = 2
 theme.taglist_font = "Monospace 12"
 
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
--- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- tasklist_[bg|fg]_[focus|urgent]
--- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- prompt_[fg|bg|fg_cursor|bg_cursor|font]
--- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
+theme.taglist_shape_focus = gears.shape.rectangle
+theme.taglist_shape_border_width_focus = 3
+theme.taglist_shape_border_color_focus = "#61AFEF"
 
--- Variables set for theming notifications:
--- notification_font
--- notification_[bg|fg]
--- notification_[width|height|margin]
--- notification_[border_color|border_width|shape|opacity]
-
--- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
 theme.menu_submenu_icon = themes_path.."default/submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
