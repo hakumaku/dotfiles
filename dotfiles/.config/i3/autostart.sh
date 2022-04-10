525766ba0e0d5b72c282dea966564964e42124c9
haku <gentlebuuny@gmail.com>
2022-04-10 11:25:01 +0900

    Update

--- dotfiles/.config/i3/autostart.sh
+++ dotfiles/.config/i3/autostart.sh
@@ -13,7 +13,7 @@ run tranmission-gtk -m
 run thunderbird
 run slack
 run redshift
-run polybar
+run gnome-keyring-daemon --daemonize --login
 
 # List idempotent commands here
 # background wallpaper
@@ -26,3 +26,16 @@ xset s off
 xset s noblank
 # The -dpms option disables DPMS (Energy Star) features.
 xset -dpms
+
+# polybar
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
