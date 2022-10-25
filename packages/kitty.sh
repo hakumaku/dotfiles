7353c9462a273ae608aca68dc80826b117b86cf3
haku <gentlebuuny@gmail.com>
2022-10-25 14:59:30 +0900

    Update

--- packages/kitty.sh
+++ packages/kitty.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 install_kitty() {
   curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-  ln -s $HOME/.local/kitty.app/bin/kitty $HOME/.local/bin
+  ln -s $HOME/.local/kitty.app/bin/kitty $HOME/.local/bin/kitty
   cp $HOME/.local/kitty.app/share/applications/kitty.desktop ${XDG_DATA_HOME}/applications/
   cp $HOME/.local/kitty.app/share/applications/kitty-open.desktop ${XDG_DATA_HOME}/applications/
   sed -i \
