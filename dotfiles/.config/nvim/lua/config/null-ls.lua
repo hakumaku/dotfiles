1362d1a0761a494ac34a340f92096018f03c97e0
haku <gentlebuuny@gmail.com>
2023-03-18 22:48:27 +0900

    Update

--- dotfiles/.config/nvim/lua/config/null-ls.lua
+++ dotfiles/.config/nvim/lua/config/null-ls.lua
@@ -1,8 +1,29 @@
 local null_ls = require("null-ls")
 local sources = {
-  null_ls.builtins.diagnostics.flake8.with({prefer_local = ".venv/bin"}),
-  null_ls.builtins.diagnostics.mypy.with({prefer_local = ".venv/bin"}),
-null_ls.builtins.code_actions.gitsigns,
+  null_ls.builtins.diagnostics.flake8.with({
+    prefer_local = ".venv/bin",
+    filetypes = {"python"}
+  }),
+  null_ls.builtins.diagnostics.mypy.with({
+    prefer_local = ".venv/bin",
+    filetypes = {"python"}
+  }),
+  null_ls.builtins.formatting.prettier.with({
+    filetypes = {
+      "javascript",
+      "typescript",
+      "css",
+      "scss",
+      "html",
+      "json",
+      "yaml",
+      "markdown",
+      "graphql",
+      "md",
+      "txt"
+    }
+  }),
+  null_ls.builtins.code_actions.gitsigns
 }
 
 null_ls.setup({sources = sources})
