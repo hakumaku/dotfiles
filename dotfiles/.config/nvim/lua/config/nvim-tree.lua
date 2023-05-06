c18c54a7d6e9fbc943510c6cb4420f3339586dad
haku <gentlebuuny@gmail.com>
2023-05-06 14:39:13 +0900

    Update

--- dotfiles/.config/nvim/lua/config/nvim-tree.lua
+++ dotfiles/.config/nvim/lua/config/nvim-tree.lua
@@ -11,8 +11,6 @@ require'nvim-tree'.setup {
   hijack_netrw = true,
   -- open the tree when running this setup function
   open_on_setup = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup = {},
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab = false,
   -- hijack the cursor in the tree to put it at the start of the filename
@@ -75,38 +73,7 @@ require'nvim-tree'.setup {
     mappings = {
       -- custom only false will merge the list with the default mappings
       -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {
-        {key = "<CR>", cb = tree_cb("edit")},
-        {key = "o", cb = tree_cb("edit")},
-        {key = "<C-[>", cb = tree_cb("dir_up")},
-        {key = "<C-]>", cb = tree_cb("cd")},
-        {key = "<C-v>", cb = tree_cb("vsplit")},
-        {key = "<C-x>", cb = tree_cb("split")},
-        {key = "<C-t>", cb = tree_cb("tabnew")},
-        {key = "<", cb = tree_cb("prev_sibling")},
-        {key = ">", cb = tree_cb("next_sibling")},
-        {key = "x", cb = tree_cb("close_node")},
-        {key = "<S-CR>", cb = tree_cb("close_node")},
-        {key = "<Tab>", cb = tree_cb("preview")},
-        {key = "I", cb = tree_cb("toggle_ignored")},
-        {key = "H", cb = tree_cb("toggle_dotfiles")},
-        {key = "R", cb = tree_cb("refresh")},
-        {key = "a", cb = tree_cb("create")},
-        {key = "d", cb = tree_cb("remove")},
-        {key = "r", cb = tree_cb("rename")},
-        {key = "<C-r>", cb = tree_cb("full_rename")},
-        {key = "-", cb = tree_cb("cut")},
-        {key = "c", cb = tree_cb("copy")},
-        {key = "p", cb = tree_cb("paste")},
-        {key = "y", cb = tree_cb("copy_name")},
-        {key = "Y", cb = tree_cb("copy_path")},
-        {key = "gy", cb = tree_cb("copy_absolute_path")},
-        {key = "[c", cb = tree_cb("prev_git_item")},
-        {key = "]c", cb = tree_cb("next_git_item")},
-        {key = "q", cb = tree_cb("close")}
-      }
+      custom_only = false
     }
   }
 }
