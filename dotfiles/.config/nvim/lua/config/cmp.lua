148b0894cac25a7027f41c3181e9c33ea590867c
haku <gentlebuuny@gmail.com>
2023-11-07 10:32:08 +0900

    Update

--- dotfiles/.config/nvim/lua/config/cmp.lua
+++ dotfiles/.config/nvim/lua/config/cmp.lua
@@ -52,7 +52,33 @@ cmp.setup {
   },
   formatting = {format = lspkind.cmp_format()},
   -- view = {entries = 'native'},
-  experimental = {ghost_text = true}
+  experimental = {ghost_text = true},
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      -- copied from cmp-under, but I don't think I need the plugin for this.
+      -- I might add some more of my own.
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find "^_+"
+        local _, entry2_under = entry2.completion_item.label:find "^_+"
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order
+    }
+  }
 }
 
 vim.api.nvim_create_autocmd("BufRead", {
