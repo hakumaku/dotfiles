310ac3efc5f3ba4604de273e9ec5fcef77413812
haku <gentlebuuny@gmail.com>
2024-04-09 10:28:03 +0900

    Update

--- dotfiles/.config/nvim/lua/config/neotest.lua
+++ dotfiles/.config/nvim/lua/config/neotest.lua
@@ -15,6 +15,7 @@ require("neotest").setup({
       -- If not provided, the path will be inferred by checking for 
       -- virtual envs in the local directory and for Pipenev/Poetry configs
       python = ".venv/bin/python"
-    })
+    }),
+    require('rustaceanvim.neotest'),
   }
 })
