2416e11153679d6d331daadab45593c93c40c081
haku <gentlebuuny@gmail.com>
2024-12-21 00:16:04 +0900

    Update

--- scripts/fcitx.sh
+++ scripts/fcitx.sh
@@ -3,22 +3,7 @@
 set -euo pipefail
 
 config_fcitx() {
-  sudo locale-gen en_US.utf8 ko_KR.utf8
-
-  msg info "executing fcitx to generate config"
-  if [[ -d "$HOME/.config/fcitx5" ]]; then
-    local conf="$HOME/.config/fcitx5"
-  else
-    local conf="$HOME/.config/fcitx"
-  fi
-
-  if [[ ! -d "$conf" ]]; then
-    fcitx -d &
-    sleep 3
-    pkill "fcitx"
-  fi
-
-  msg info "configuring fcitx"
+  local conf="$HOME/.config/fcitx5"
   # Set input method
   local profile="$conf/profile"
   sed -Ei "s/#(IMName=)/\1Hangul/" "$profile"
@@ -33,10 +18,6 @@ config_fcitx() {
   # Disable ctrl+; key in fcitx-clipboard.config
   local clipboard="$conf/conf/fcitx-clipboard.config"
   sed -Ei "s/#(TriggerKey=).*/\1/" "$clipboard"
-
-  # im-config -n fcitx
-  msg info "configuring input method"
-  sudo sed -i "s/run_im (.*)/run_im fcitx/" /etc/X11/xinit/xinputrc
 }
 
 config_fcitx
