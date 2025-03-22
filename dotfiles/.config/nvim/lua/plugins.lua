1e5b9678e99e0568231fd0114c3d1af918a9902d
haku <gentlebuuny@gmail.com>
2025-03-22 12:03:53 +0900

    Update

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -244,7 +244,6 @@ require("lazy").setup({
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
-      scroll = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
       image = { enabled = true }
