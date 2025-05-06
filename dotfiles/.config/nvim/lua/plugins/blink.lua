965c325e78d760782ba1c48f6a771b4b7793274a
haku <gentlebuuny@gmail.com>
2025-05-06 18:29:09 +0900

    Update

--- dotfiles/.config/nvim/lua/plugins/blink.lua
+++ dotfiles/.config/nvim/lua/plugins/blink.lua
@@ -1,78 +1,77 @@
 return {
   {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     dependencies = {
-      "rafamadriz/friendly-snippets",
       "L3MON4D3/LuaSnip",
-      version = "v2.*"
+      version = "v2.*",
     },
-    version = '1.*',
+    version = "1.*",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = {preset = 'default'},
+      keymap = { preset = "default" },
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
+        nerd_font_variant = "mono",
       },
       -- (Default) Only show the documentation popup when manually triggered
       completion = {
-        documentation = {auto_show = false},
+        documentation = { auto_show = false },
         menu = {
           auto_show = true,
           draw = {
             columns = {
-              {"kind_icon", gap = 1},
-              {"label", "label_description", gap = 1}
+              { "kind_icon", gap = 1 },
+              { "label", "label_description", gap = 1 },
             },
             components = {
-              label = {width = {fill = true, max = 32}},
+              label = { width = { fill = true, max = 32 } },
               label_description = {
-                width = {max = 28},
+                width = { max = 28 },
                 text = function(ctx)
                   return ctx.label_description
                 end,
-                highlight = 'BlinkCmpLabelDescription'
+                highlight = "BlinkCmpLabelDescription",
               },
               source_name = {
-                width = {max = 16},
+                width = { max = 16 },
                 text = function(ctx)
                   return ctx.source_name
                 end,
-                highlight = 'BlinkCmpSource'
-              }
-            }
-          }
+                highlight = "BlinkCmpSource",
+              },
+            },
+          },
         },
-        ghost_text = {show_with_menu = true}
+        ghost_text = { show_with_menu = true },
       },
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = {'lsp', 'path', 'snippets', 'buffer', 'cmdline'},
-        per_filetype = {sql = {'snippets', 'dadbod', 'buffer'}},
+        default = { "lsp", "path", "snippets", "buffer", "cmdline" },
+        per_filetype = { sql = { "snippets", "dadbod", "buffer" } },
         -- add vim-dadbod-completion to your completion providers
         providers = {
-          dadbod = {name = "Dadbod", module = "vim_dadbod_completion.blink"}
-        }
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
       },
-      snippets = {preset = 'luasnip'},
-      fuzzy = {implementation = "prefer_rust_with_warning"}
+      snippets = { preset = "luasnip" },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
       -- pts_extend = { "sources.default" }
-    }
+    },
   },
   {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     config = function()
-      require('mason').setup({
-        ui = {border = 'rounded'},
+      require("mason").setup({
+        ui = { border = "rounded" },
         registries = {
-          'github:mason-org/mason-registry',
-          'github:Crashdummyy/mason-registry'
-        }
+          "github:mason-org/mason-registry",
+          "github:Crashdummyy/mason-registry",
+        },
       })
-    end
+    end,
   },
   {
     "L3MON4D3/LuaSnip",
@@ -80,23 +79,34 @@ return {
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_vscode").lazy_load({
+            paths = { vim.fn.stdpath("config") .. "/snippets" },
+          })
+        end,
+      },
+    },
     config = function(_, opts)
       if opts then
         require("luasnip").config.setup(opts)
       end
       vim.tbl_map(function(type)
         require("luasnip.loaders.from_" .. type).lazy_load()
-      end, {"vscode", "snipmate", "lua"})
+      end, { "vscode", "snipmate", "lua" })
       -- friendly-snippets - enable standardized comments snippets
-      require("luasnip").filetype_extend("typescript", {"tsdoc"})
-      require("luasnip").filetype_extend("javascript", {"jsdoc"})
-      require("luasnip").filetype_extend("lua", {"luadoc"})
-      require("luasnip").filetype_extend("python", {"pydoc"})
-      require("luasnip").filetype_extend("rust", {"rustdoc"})
-      require("luasnip").filetype_extend("c", {"cdoc"})
-      require("luasnip").filetype_extend("cpp", {"cppdoc"})
-      require("luasnip").filetype_extend("kotlin", {"kdoc"})
-      require("luasnip").filetype_extend("sh", {"shelldoc"})
-    end
-  }
+      require("luasnip").filetype_extend("typescript", { "tsdoc" })
+      require("luasnip").filetype_extend("javascript", { "jsdoc" })
+      require("luasnip").filetype_extend("lua", { "luadoc" })
+      require("luasnip").filetype_extend("python", { "pydoc" })
+      require("luasnip").filetype_extend("rust", { "rustdoc" })
+      require("luasnip").filetype_extend("c", { "cdoc" })
+      require("luasnip").filetype_extend("cpp", { "cppdoc" })
+      require("luasnip").filetype_extend("kotlin", { "kdoc" })
+      require("luasnip").filetype_extend("sh", { "shelldoc" })
+    end,
+  },
 }
--- dotfiles/.config/nvim/lua/plugins/conform.lua
+++ dotfiles/.config/nvim/lua/plugins/conform.lua
@@ -1,21 +1,31 @@
 return {
-  'stevearc/conform.nvim',
-  event = {"BufWritePre"},
-  cmd = {"ConformInfo"},
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
   keys = {
     {
       -- Customize or remove this keymap to your liking
       "<leader>f",
       function()
-        require("conform").format({async = true})
+        require("conform").format({ async = true })
       end,
       mode = "",
-      desc = "Format buffer"
-    }
+      desc = "Format buffer",
+    },
   },
   opts = {
     formatters = {
-      rustfmt = {prepend_args = {"+nightly"}},
+      rustfmt = { prepend_args = { "+nightly" } },
+      stylua = {
+        prepend_args = {
+          "--column-width",
+          100,
+          "--indent-type",
+          "spaces",
+          "--indent-width",
+          "2",
+        },
+      },
       sqlfluff = {
         command = "sqlfluff",
         args = {
@@ -23,24 +33,24 @@ return {
           "--config",
           vim.fn.stdpath("config") .. "/.sqlfluff",
           "--disable-progress-bar",
-          "-"
+          "-",
         },
-        stdin = true
-      }
+        stdin = true,
+      },
     },
     formatters_by_ft = {
-      lua = {"lua-format"},
-      python = {"black", "ruff"},
-      rust = {"rustfmt"},
-      sql = {"sqlfluff"},
+      lua = { "stylua" },
+      python = { "black", "ruff" },
+      rust = { "rustfmt" },
+      sql = { "sqlfluff" },
       -- Use the "_" filetype to run formatters on filetypes that don't
       -- have other formatters configured.
-      ["_"] = {"trim_whitespace"}
+      ["_"] = { "trim_whitespace" },
     },
     format_on_save = {
       -- These options will be passed to conform.format()
       timeout_ms = 500,
-      lsp_format = "fallback"
-    }
-  }
+      lsp_format = "fallback",
+    },
+  },
 }
